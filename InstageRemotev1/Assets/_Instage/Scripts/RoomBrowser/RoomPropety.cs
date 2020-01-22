5ae44fe0a93dc0cf568a14d83e4019757128fb06
Pier Boivin <pier@instage.io>
2020-01-22 11:28:00 -0500

    Readjusted ui to size with iphone X

---
+++ InstageRemotev1/Assets/_Instage/Scripts/RoomBrowser/RoomPropety.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This class ensures all room properties will always be spelt correctly
+public class RoomPropety
+{
+    public const string Scenario = "SCE";
+    public const string Company = "COM";
+    public const string Location = "LOC";
+}
